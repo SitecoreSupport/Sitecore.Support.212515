9cdd15e48dd41405e4b80fb29b151a54a6c07cfb
DK\IEKA <ieka@sitecore.net>
2018-09-13 13:59:38 +0300

    Prevents the Mixed Content error in Workbox

---
+++ src/Sitecore.Support.212515/Shell/Applications/Workbox/WorkboxForm.cs
@@ -0,0 +1,30 @@
+﻿using Sitecore.Text;
+using Sitecore.Web;
+using System.Collections.Generic;
+
+namespace Sitecore.Support.Shell.Applications.Workbox
+{
+  public class WorkboxForm: Sitecore.Shell.Applications.Workbox.WorkboxForm
+  {
+    protected override void Refresh()
+    {
+      Refresh(null);
+    }
+
+    protected new void Refresh(Dictionary<string, string> urlArguments)
+    {
+      var url = new UrlString(WebUtil.GetRawUrl());
+      url["reload"] = "1";
+
+      if (urlArguments != null)
+      {
+        foreach (var urlArgument in urlArguments)
+        {
+          url[urlArgument.Key] = urlArgument.Value;
+        }
+      }
+
+      Context.ClientPage.ClientResponse.SetLocation(url.ToString());
+    }
+  }
+}
